ee5358ac80b59cb78205555076c2cd90077e7a68
snrathod <snrathod@ncsu.edu>
2015-08-05 09:32:40 -0400

    To parse error messages

---
+++ src/main/java/com/joulebug/readability/WegoViewMeterError.java
@@ -0,0 +1,17 @@
+package com.joulebug.readability;
+
+/**
+ * Created by shraddharathod on 8/4/15.
+ */
+public class WegoViewMeterError {
+
+    private String error;
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}
