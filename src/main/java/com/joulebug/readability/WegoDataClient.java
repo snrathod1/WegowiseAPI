56fae3451a75aab65ffb4da118167b138c1ff17a
snrathod <snrathod@ncsu.edu>
2015-08-05 09:31:52 -0400

    Added Http error handling

--- src/main/java/com/joulebug/readability/WegoDataClient.java
+++ src/main/java/com/joulebug/readability/WegoDataClient.java
@@ -28,17 +28,6 @@ public class WegoDataClient {
     public WegoDataClient(String key, String secret) {
         this.oauthKey = key;
         this.oauthSecret = secret;
-        /*
-        try {
-            this.oauthKey = key;
-            this.oauthSecret = secret;
-        } catch (IllegalArgumentException e) {
-            System.out.println("Exception thrown:" + e);
-            System.out.println("Please enter a key and a secret.");
-            System.exit(0);
-        }
-        */
-
 
     }
     //token request, verification, and data retrieval; parsing data
@@ -51,12 +40,12 @@ public class WegoDataClient {
         Request(service);
         Verification();
         Access(service);
-        getDataOnlyMeter(service);
-        getWegoDataMeterWithID(service, ID);
-        getViewLogin(service, ID);
-        getMeterRawData(service, ID);
-        getMeterRawDataPoint(service, ID);
-        getMeterRawDatum(service, ID);
+        getDataOnlyMeter();
+        getWegoDataMeterWithID();
+        getViewLogin();
+        getMeterRawData();
+        getMeterRawDataPoint();
+        getMeterRawDatum();
     }
     //Request
     public void Request(OAuthService service) {
@@ -109,122 +98,75 @@ public class WegoDataClient {
         System.out.println();
         this.accessToken = accessToken;
     }
+
+    protected<T> T get(OAuthService service,Class<T> name, String end) {
+        T result = null;
+        OAuthRequest request = new OAuthRequest(Verb.GET, PROTECTED_RESOURCE_URL + end);
+        service.signRequest(accessToken, request);
+        Response response = request.send();
+
+        if (response.isSuccessful()) {
+            Gson gson = new Gson();
+            String json = response.getBody();
+            T data = gson.fromJson(json, name);
+            return data;
+        } else {
+            System.out.println("Status Code is: " + response.getCode());
+            Gson gSon = new Gson();
+            String jSon = response.getBody();
+            WegoViewMeterError message = gSon.fromJson(jSon, WegoViewMeterError.class);
+        }
+        return result;
+    }
     //Parsing the code
 
-    public WegoDataMeter[] getDataOnlyMeter(OAuthService service) {
-        WegoDataMeter[] result = null;
-        //Accessing protected source
-        OAuthRequest request = new OAuthRequest(Verb.GET, PROTECTED_RESOURCE_URL + "meters");
-        service.signRequest(accessToken, request);
-        Response response = request.send();
-        //System.out.print(response.getBody());
+    public WegoDataMeter[] getDataOnlyMeter() {
         System.out.println("DataOnlyMeter: ");
+        return get(service, WegoDataMeter[].class, "meters", null);
+////            for (WegoDataMeter parsed : data) {
+////                parsed.getCoverage();
+////                parsed.getData_type();
+////                parsed.getFor_heating();
+////                this.ID = parsed.getId();
+////                parse
+////                parsed();
+////                parseame();
+////
+////
 
-        if (response.isSuccessful()) {
-            Gson gson = new Gson();
-            String json = response.getBody();
-
-            WegoDataMeter[] data = gson.fromJson(json, WegoDataMeter[].class);
-            for (WegoDataMeter parsed : data) {
-                parsed.getCoverage();
-                parsed.getData_type();
-                parsed.getFor_heating();
-                this.ID = parsed.getId();
-                parsed.getNotes();
-                parsed.getUtility_company().getId();
-                parsed.getUtility_company().getName();
-
-
-            }
-            result = data;
-        } else {
-            System.out.println("Status Code is: " + response.getCode());
-        }
-        System.out.println();
-        return result;
     }
-    public WegoDataMeter getWegoDataMeterWithID(OAuthService service, Integer IDin) {
-        WegoDataMeter result = null;
-        //Accessing protected source
-        OAuthRequest request = new OAuthRequest(Verb.GET, PROTECTED_RESOURCE_URL + "meters/" + IDin);
-        service.signRequest(accessToken, request);
-        Response response = request.send();
+    public WegoDataMeter getWegoDataMeterWithID(Integer IDin) {
         System.out.println("DataMeterWithID: ");
+        return get(service, WegoDataMeter.class, "meters" + IDin);
 
 
-        if (response.isSuccessful()) {
-            Gson gson = new Gson();
-            String json = response.getBody();
-
-            WegoDataMeter data = gson.fromJson(json, WegoDataMeter.class);
-
-            data.getCoverage();
-            data.getData_type();
-            data.getFor_heating();
-            data.getId();
-            data.getNotes();
-            data.getUtility_company().getId();
-            data.getUtility_company().getName();
-
-            result = data;
-
-        } else {
-            System.out.println("Status Code is: " + response.getCode());
-        }
-        System.out.println();
-
-        return result;
+           // data.getCoverage();
+           // data.getData_type();
+           // data.getFor_heating();
+            //data.getId();
+            //data.getNotes();
+           // data.getUtility_company().getId();
+//            data.getUtility_company().getName();
 
     }
-    public ViewUtilityLogin getViewLogin(OAuthService service, Integer IDin) {
-        ViewUtilityLogin result = null;
-        //Accessing protected source
-        OAuthRequest request = new OAuthRequest(Verb.GET, PROTECTED_RESOURCE_URL + "utility_logins/" + IDin);
-        service.signRequest(accessToken, request);
-        Response response = request.send();
-        System.out.println("ViewLogin: ");
+    public ViewUtilityLogin getViewLogin(Integer IDin) {
+        return get(service, ViewUtilityLogin.class, "utility_logins/" + IDin);
 
 
-        if (response.isSuccessful()) {
-            Gson gson = new Gson();
-            String json = response.getBody();
+            //data.getId();
+            //data.getStatus();
+            //data.getUsername();
+            //data.getUtility_company().getName();
+            //data.getAccount_numbers();
 
-            ViewUtilityLogin data = gson.fromJson(json, ViewUtilityLogin.class);
-
-            data.getId();
-            data.getStatus();
-            data.getUsername();
-            data.getUtility_company().getName();
-            data.getAccount_numbers();
-
-            result = data;
-
-        } else {
-            System.out.println("Status Code is: " + response.getCode());
-        }
-        System.out.println();
-
-        return result;
     }
 
 
-    public WegoDataRawData[] getMeterRawData (OAuthService service, Integer IDin) {
-        WegoDataRawData[] result = null;
-        //Accessing protected source
-        OAuthRequest request = new OAuthRequest(Verb.GET, PROTECTED_RESOURCE_URL + "meters/" + IDin + "/raw_data");
-        service.signRequest(accessToken, request);
-        Response response = request.send();
+    public WegoDataRawData[] getMeterRawData (Integer IDin) {
         System.out.println("MeterRawData: ");
+        return get(service, WegoDataRawData[].class, "meters/" + IDin + "/raw_data");
 
-
-        if (response.isSuccessful()) {
-            Gson gson = new Gson();
-            String json = response.getBody();
-
-            WegoDataRawData[] data = gson.fromJson(json, WegoDataRawData[].class);
-
-            for (WegoDataRawData parsed : data) {
-                parsed.getDelivery_charge();
+                /*parsed.getDelivery_charge();
                 parsed.getEnd_date();
                 parsed.getFuel_charge();
                 parsed.getTotal_charge();
@@ -239,32 +181,15 @@ public class WegoDataClient {
                 parsed.getOff_peak_kwh();
                 parsed.getPeak_charge();
                 parsed.getPeak_kwh();
-            }
-            result = data;
-
-        } else {
-            System.out.println("Status Code is: " + response.getCode());
-        }
-        System.out.println();
-        return result;
+                **/
 
     }
-    public WegoDataRawData getMeterRawDataPoint (OAuthService service, Integer IDin) {
-        WegoDataRawData result = null;
-        //Accessing protected source
-        OAuthRequest request = new OAuthRequest(Verb.GET, PROTECTED_RESOURCE_URL + "meters/" + IDin + "/raw_data/" + IDin);
-        service.signRequest(accessToken, request);
-        Response response = request.send();
+    public WegoDataRawData getMeterRawDataPoint (Integer IDin) {
         System.out.println("MeterRawDataPoint: ");
+        return get(service, WegoDataRawData.class, "meters/" + IDin + "/raw_data" + IDin);
 
 
-        if (response.isSuccessful()) {
-            Gson gson = new Gson();
-            String json = response.getBody();
-
-            WegoDataRawData data = gson.fromJson(json, WegoDataRawData.class);
-
-            data.getDelivery_charge();
+            /*data.getDelivery_charge();
             data.getEnd_date();
             data.getFuel_charge();
             data.getTotal_charge();
@@ -279,31 +204,15 @@ public class WegoDataClient {
             data.getOff_peak_kwh();
             data.getPeak_charge();
             data.getPeak_kwh();
+            **/
 
-            result = data;
-        } else {
-            System.out.println("Status Code is: " + response.getCode());
-        }
-        System.out.println();
-
-        return result;
 
     }
-    public WegoDataRawData getMeterRawDatum (OAuthService service, Integer IDin) {
-        WegoDataRawData result = null;
-        //Accessing protected source
-        OAuthRequest request = new OAuthRequest(Verb.GET, PROTECTED_RESOURCE_URL + "meters/" + IDin + "/latest_datum");
-        service.signRequest(accessToken, request);
-        Response response = request.send();
+    public WegoDataRawData getMeterRawDatum (Integer IDin) {
         System.out.println("MeterRawDatum: ");
+        return get(service, WegoDataRawData.class, "meters/" + IDin + "/latest_datum");
 
-
-        if (response.isSuccessful()) {
-            Gson gson = new Gson();
-            String json = response.getBody();
-
-            WegoDataRawData data = gson.fromJson(json, WegoDataRawData.class);
-
+/*
             data.getDelivery_charge();
             data.getEnd_date();
             data.getFuel_charge();
@@ -319,13 +228,7 @@ public class WegoDataClient {
             data.getOff_peak_kwh();
             data.getPeak_charge();
             data.getPeak_kwh();
-
-            result = data;
-        } else {
-            System.out.println("Status Code is: " + response.getCode());
-        }
-        System.out.println();
-        return result;
+            **/
 
     }
 
